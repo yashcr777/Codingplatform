d97363fc47059c592d78d55df2a7bed585f784a4
yashcr777 <yashagarwalnobody@gmail.com>
2026-04-04 09:22:21 +0530

    remove unwnred coe

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,13 +17,13 @@ app.get("/health",(req,res)=>{
 app.get("/books",(req,res)=>{
     res.status(200).json({msg:"api is up and running"})
 })
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+// if(ENV.NODE_ENV==="production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    });
-}
+//     app.get("{*any}",(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     });
+// }
 const startServer=async()=>{
     try{
         await connectDB();
