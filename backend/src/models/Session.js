dad3a62c44f7696b75d17e77b11a69614471ba96
yashcr777 <yashagarwalnobody@gmail.com>
2026-04-08 12:06:22 +0530

    updated the model

--- backend/src/models/Session.js
+++ backend/src/models/Session.js
@@ -1,38 +1,40 @@
 import mongoose from "mongoose";
 
-const sessionSchema=new mongoose.Schema({
-        problem:{
-            type:String,
-            required:true,
-        },
-        difficulty:{
-            type:String,
-            enum:["easy","medium","hard"],
-            required:true
-        },
-        host:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-            required:true
-        },
-        participant:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-        },
-        status:{
-            type:String,
-            enum:["active","completed"],
-            default:"active"
-        },
-        callId:{
-            type:String,
-            default:""
-        }
+const sessionSchema = new mongoose.Schema(
+  {
+    problem: {
+      type: String,
+      required: true,
     },
-    {timestamps:true}
+    difficulty: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
+    },
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    participant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ["active", "completed"],
+      default: "active",
+    },
+    // stream video call ID
+    callId: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
 );
 
-
-const Session=mongoose.model("Session",sessionSchema)
+const Session = mongoose.model("Session", sessionSchema);
 
 export default Session;
