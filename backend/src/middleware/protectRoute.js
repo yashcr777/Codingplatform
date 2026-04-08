8c2193315aaf7694789e8277e507dff1ecd8f51e
yashcr777 <yashagarwalnobody@gmail.com>
2026-04-08 11:42:23 +0530

    protectroute

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,24 +1,26 @@
-import {requireAuth} from '@clerk/express'
+import { requireAuth } from "@clerk/express";
+import User from "../models/User.js";
 
-import User from '../models/User.js';
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth().userId;
 
-export const protectRoute=[
-    requireAuth(),
-    async(req,res,next)=>{
-        try{
-            const clerkId=req.auth().userId;
-            if(!clerkId)return res.status(401).json({msg:"Unauthorized - invalid token"});
-            const user=await User.findOne({clerkId});
+      if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
 
-            if(!user)return res.status(404).json({msg:"USer not found"})
+      // find user in db by clerk ID
+      const user = await User.findOne({ clerkId });
 
-            req.user=user
+      if (!user) return res.status(404).json({ message: "User not found" });
 
-            next();
+      // attach user to req
+      req.user = user;
 
-        } catch(error){
-            console.error("Error in protectRoute middleware",error);
-            res.status(500).json({message:"INternal Server Error"});
-        }
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware", error);
+      res.status(500).json({ message: "Internal Server Error" });
     }
-]
+  },
+];
