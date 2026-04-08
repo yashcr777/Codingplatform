7e3cea393b1f20803a8803265910007b5bfd6fab
yashcr777 <yashagarwalnobody@gmail.com>
2026-04-08 12:20:26 +0530

    upated the model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema=new mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required:true,
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        profileImage:{
-            type:String,
-            defaullt:""
-        },
-        clerkId:{
-            type:String,
-            required:true,
-            unique:true,
-        }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {timestamps:true}
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true } // createdAt, updatedAt
 );
 
-const User=mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
